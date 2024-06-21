4b89b7c3e783a4dea36bb88fb20363a46409ebf6
antoniocarlosvr <antonio.junior@vrsoft.com.br>
2024-06-21 14:29:35 -0300

    Adding Prettier Writing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
 import { PostModule } from './post/post.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
